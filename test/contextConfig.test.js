e4aac250d322e630ea5761aa5b32939bea12e493
Denis Shelest <axules@gmail.com>
2018-10-23 23:51:48 +0300

    add tests for config

---
+++ test/contextConfig.test.js
@@ -0,0 +1,30 @@
+import { InitProvider } from '../src';
+import app, { actions } from './app.context';
+
+describe('InitProvider', () => {
+  test('should return object with Provider and connect values', () => {
+    const iP = InitProvider({ app }, { app: actions }, { debug: true });
+
+    expect(Object.keys(iP)).toEqual(['Provider', 'connect']);
+    expect(iP.Provider).toBeInstanceOf(Function);
+    expect(iP.connect).toBeInstanceOf(Function);
+  });
+
+  test('should throw error about actions in initProvider', () => {
+    try {
+      InitProvider({ app }, { app: { a: null } }, { debug: true });
+    } catch(e) {
+      expect(e.message).toEqual('initProvider:"app.a" should be function, it is "object"');
+    }
+  });
+
+  test('should throw error about actions in connect', () => {
+    const iP = InitProvider({ app }, { app: actions }, { debug: true });
+
+    try {
+      iP.connect(() => {}, { a: 999 })(() => null);
+    } catch(e) {
+      expect(e.message).toEqual('initConnect:"a" should be function, it is "number"');
+    }
+  });
+});
